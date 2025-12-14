64d75365478fd15091da60a9859111aeed763f81
c2nsored <c2nsored@gmail.com>
2025-12-14 16:30:48 +0900

    Security: Move Firebase config to environment variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // TODO: Replace with your actual Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAgHltO02u8mjzqYudFrXL9sOcLYoLqAfo",
-  authDomain: "mixshop-jp.firebaseapp.com",
-  projectId: "mixshop-jp",
-  storageBucket: "mixshop-jp.firebasestorage.app",
-  messagingSenderId: "1034024388578",
-  appId: "1:1034024388578:web:d5be1881a9e263a7389bbb",
-  measurementId: "G-WRXXCJPZYR"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
